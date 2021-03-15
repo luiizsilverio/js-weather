1db4006fcb282ae4031b62e0230fd3dc280a2551
luiizsilverio <luiiz.silverio@gmail.com>
2021-03-15 14:05:31 -0300

    js-weather

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const cityForm = document.querySelector('[data-js="change-location"]')
+const cityCard = document.querySelector('[data-js="city-card"]')
+let timeImg = document.querySelector('[data-js="time"]')
+const timeIcon = document.querySelector('[data-js="time-icon"]')
+
+const buscaCidade = async (cityName) => {  
+  const { Key, LocalizedName } = await getCityData(cityName)
+
+  const { WeatherText, Temperature, IsDayTime, WeatherIcon } = await getCityWeather(Key);
+
+  const weather = document.querySelector('[data-js="weather-details"]');
+  const cidade = weather.querySelector('.cidade');
+  const clima = weather.querySelector('.clima');
+  const temperatura = weather.querySelector('.temperatura > span');
+  
+  cidade.textContent = LocalizedName;
+  clima.textContent = WeatherText;
+  temperatura.textContent = Temperature.Metric.Value;
+  timeIcon.innerHTML = `<img src="./src/icons/${WeatherIcon}.svg" />`
+  timeImg.src = IsDayTime ? './src/day.svg' : './src/night.svg'
+  
+  if (Key) {
+    localStorage.setItem('cidade', cityName);
+  }
+
+  cityCard.classList.remove('hidden')  
+  cityForm.reset()
+}
+
+cityForm.addEventListener('submit', async event => {
+  event.preventDefault()
+  
+  buscaCidade(event.target.city.value)
+})
+
+const lastCity = localStorage.getItem('cidade')
+
+if (lastCity) {
+  cityForm.city.value = lastCity
+  buscaCidade(lastCity)
+}
---
+++ js/weather.js
@@ -0,0 +1,84 @@
+// developer.accuweather.com
+// API Key = MqWwn3UvDmn20U4QZHZBSotTAbx3LDUG
+// 
+// API City Search:
+// http://dataservice.accuweather.com/locations/v1/cities/search
+// Query Parameters: apikey, q (texto a procurar)
+// Outros parâmetros não obrigatórios: language = "pt-br", 
+// details = false, offset = 25
+//
+// API Current Conditions:
+// http://dataservice.accuweather.com/currentconditions/v1/
+// Query Parameters: apikey, language (opcional)
+
+const APIkey = 'ezfnfGbOcLOhuSuGPUwvFpos2cq9NEzv'
+
+const getCityUrl = city =>
+   `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIkey}&q=${city}`
+  
+/********************************************
+ * getCityData (cityName)
+ ********************************************/
+const getCityData = async cityName => {
+  try {
+    const cityUrl = getCityUrl(cityName)
+    const response = await fetch(cityUrl)
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados')
+    }
+
+    // destructurin de array: armazena o primeiro item em cityData
+    const [cityData] = await response.json()
+    return cityData
+
+  } catch ({ name, message }) {
+    alert(`${name}: ${message}`)
+  }
+}
+
+const getWeatherUrl = key =>
+   `http://dataservice.accuweather.com/currentconditions/v1/${key}?apikey=${APIkey}&language=pt-br`
+
+/********************************************
+ * getCityWeather (cityKey)
+ ********************************************/
+const getCityWeather = async (cityKey) => {
+  try {
+    if (!cityKey) {      
+      throw new Error('Não foi possível obter os dados')
+    }
+
+    const weatherUrl = getWeatherUrl(cityKey)
+    const response = await fetch(weatherUrl)
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados')
+    }
+
+    // destructurin de array: armazena o primeiro item em cityData
+    const [cityWeatherData] = await response.json()
+    return cityWeatherData
+
+  } catch ({ name, message }) {
+    alert(`${name}: ${message}`)
+  }
+}
+
+const inicCity = async (cityName) => {
+  const cityData = await getCityData(cityName);
+    
+  if (cityData) {
+    const { Key } = cityData
+    console.log(Key)
+    cityWeatherData = await getCityWeather(Key);
+    console.log(cityWeatherData)
+  }
+}
+
+//inicCity('campinas');
+
+
+
+
+
